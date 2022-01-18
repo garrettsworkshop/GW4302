08b1c317c119a4e5193c73c409381de51243e5be
Zane Kaminski <zanekaminski@gmail.com>
2022-01-18 12:15:12 -0500

    More DMA sequencer control

--- cpld/DMASeq.v
+++ cpld/DMASeq.v
@@ -4,14 +4,14 @@ module DMASeq(
 	
 	input BA,
 	
-	output RAMRD,
-	output RAMWR,
+	output reg RAMRD,
+	output reg RAMWR,
 	
 	input Equal,
 	input Execute,
 	
 	output DMA,
-	output DMARW,
+	output reg DMARW,
 	
 	output reg RegReset,
 	
@@ -25,6 +25,7 @@ module DMASeq(
 	
 	reg Executing;
 	reg SwapState;
+	assign DMA = Executing;
 	
 	always @(negedge PHI2) begin
 		if (Executing) begin
@@ -39,9 +40,7 @@ module DMASeq(
 					end
 				end 2'b10: begin // Swap
 					if (SwapState) begin
-						if (Length1) begin
-						end else begin
-						end
+						SwapState <= 0;
 					end else begin
 						if (Length1) begin
 						end else begin
@@ -54,29 +53,21 @@ module DMASeq(
 				end
 			endcase
 		end else if (Execute) begin
+			Executing <= 1;
+			SwapState <= 1;
 			case (XferType[1:0])
 				2'b00: begin // C64-to-REU
-					if (Length1) begin
-					end else begin
-					end
+					RAMRD <= 0;
+					RAMWR <= 0;
 				end 2'b01: begin // REU-to-C64
-					if (Length1) begin
-					end else begin
-					end
+					RAMRD <= 1;
+					RAMWR <= 0;
 				end 2'b10: begin // Swap
-					if (SwapState) begin
-						if (Length1) begin
-						end else begin
-						end
-					end else begin
-						if (Length1) begin
-						end else begin
-						end
-					end
+					RAMRD <= 1;
+					RAMWR <= 0;
 				end 2'b11: begin // Verify
-					if (Length1) begin
-					end else begin
-					end
+					RAMRD <= 1;
+					RAMWR <= 0;
 				end
 			endcase
 		end
